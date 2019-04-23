385d234e68c8ac56614b310b43d2f491b17f0942
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-23 00:46:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ attendees = ["Amy", "Emily", "Vincent"]
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-    "Hello, my name is #{attandee}"
+    "Hello, my name is #{attandee}."
 
 end
